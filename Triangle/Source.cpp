abadba2f00652a5ac75b31de82dbf92565a0f66d
Abdul Basit <abasitm.1993@gmail.com>
2017-11-11 20:40:05 +0500

    Initial commit

---
+++ Triangle/Source.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <glut.h>
+
+void render(void);
+void keyboard(unsigned char key, int x, int y);
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(640, 480);
+	glutCreateWindow("OpenGL Triangle");
+	glutDisplayFunc(render);
+
+	glutMainLoop();
+	
+	return 1;
+}
+
+void render()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glBegin(GL_TRIANGLES);
+		glColor3f(1.0f, 0.0f, 0.0f); glVertex3f(-0.5f, -0.5f, 0.0f);
+		glColor3f(0.0f, 1.0f, 0.0f); glVertex3f(0.0f, 0.5f, 0.5f);
+		glColor3f(0.0f, 0.0f, 1.0f); glVertex3f(0.0f, 0.0f, -0.5f);
+	glEnd();
+
+	glutSwapBuffers();
+}
